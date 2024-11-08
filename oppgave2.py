e73a3c50aab9085e4cc31411152b9993a14ba58a
PhillipOmmundsen <phillip24@iktfag.com>
2024-11-08 10:19:18 +0100

    Initial commit

---
+++ oppgave2.py
@@ -0,0 +1,10 @@
+tall = 1
+while tall >= 1 and tall <= 20:
+    print(tall)
+    tall +=2
+
+tall = 0
+while tall >= 0 and tall <= 20:
+    print(tall)
+    tall +=2
+    
---
+++ oppgave3.py
@@ -0,0 +1,3 @@
+for tall in range (50,100):
+    if tall % 2 != 0:
+        print(tall)
